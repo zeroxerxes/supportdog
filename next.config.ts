36d069179d1ab64911726047b58facb444a13933
zeroxerxes <99093016+zeroxerxes@users.noreply.github.com>
2025-12-24 00:51:57 +0100

    init2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: 'C:/Users/Zero/Desktop/ESD/app',
+    root: 'C:/Users/Zero/Desktop/ESD',
   },
 };
 
